1fe5a2ca0ebeae2a92ac2b882b4617605b6fbee0
Sachini Epa <sachi.epa96@gmail.com>
2024-11-16 23:46:14 -0800

    Adding Patient History related UIs

--- app/src/main/java/com/example/healthconnect/AddPatientHistory.java
+++ app/src/main/java/com/example/healthconnect/AddPatientHistory.java
@@ -1,6 +1,9 @@
 package com.example.healthconnect;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,5 +19,13 @@ public class AddPatientHistory extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_add_patient_history);
 
+        Button btnBack = (Button) findViewById(R.id.btnBack);
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(AddPatientHistory.this, ViewPatientHistory.class));
+            }
+        });
     }
 }
--- app/src/main/java/com/example/healthconnect/ViewPatientHistory.java
+++ app/src/main/java/com/example/healthconnect/ViewPatientHistory.java
@@ -1,6 +1,10 @@
 package com.example.healthconnect;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.sax.StartElementListener;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +19,13 @@ public class ViewPatientHistory extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_view_patient_history);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        Button btnPlus = (Button) findViewById(R.id.btnPlus);
+        btnPlus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(ViewPatientHistory.this, AddPatientHistory.class));
+            }
         });
     }
 }
