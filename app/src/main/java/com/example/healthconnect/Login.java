cf14f4503809df2bc9e0c2346c01b2a1e0918fa8
leshan <leshan.chathuranga@gmail.com>
2024-11-27 23:40:06 -0700

    initial

---
+++ app/src/main/java/com/example/healthconnect/Login.java
@@ -0,0 +1,27 @@
+package com.example.healthconnect;
+
+
+import android.os.Bundle;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login);
+
+        EditText txtDoctorID = (EditText) findViewById(R.id.txtPractitionerID);
+        EditText txtPassword = (EditText) findViewById(R.id.txtPassword);
+
+
+
+    }
+}
